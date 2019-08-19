9ca689bdb70ddfcc66a61a89c89ab8e8392d6906
alauhy <751961605@qq.com>
2019-08-19 21:24:16 +0800

    [LXY]:write test given item name sulfuras,hand of ragnaros and quality 50

--- src/test/java/gilded_rose/GildedRoseTest.java
+++ src/test/java/gilded_rose/GildedRoseTest.java
@@ -20,13 +20,23 @@ public class GildedRoseTest {
 
     }
     @Test
-    public void should_quality_49_when_updateQuality_given_normal_item_sellIn_10_and_quality_is_0(){
+    public void should_quality_0_when_updateQuality_given_normal_item_sellIn_10_and_quality_is_0(){
         Item item = new Item("oocl full stack",10,0);
         GildedRose gildedRose = new GildedRose(singletonList(item));
 
         gildedRose.updateQuality();
 
         Assert.assertEquals(0,item.getQuality());
+
+    }
+    @Test
+    public void should_quality_50_when_updateQuality_given_item_name_Sulfuras_Hand_of_Ragnaros_sellIn_10_and_quality_is_50(){
+        Item item = new Item("Sulfuras, Hand of Ragnaros",10,50);
+        GildedRose gildedRose = new GildedRose(singletonList(item));
+
+        gildedRose.updateQuality();
+
+        Assert.assertEquals(50,item.getQuality());
 
     }
 }
