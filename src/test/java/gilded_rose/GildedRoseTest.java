a4fe888e98405c130f2e3d34a0b308e439663e18
alauhy <751961605@qq.com>
2019-08-19 21:33:09 +0800

    [LXY]:write test given item name Backstage_passes_to_a_TAFKAL80ETC_concert

--- src/test/java/gilded_rose/GildedRoseTest.java
+++ src/test/java/gilded_rose/GildedRoseTest.java
@@ -49,4 +49,34 @@ public class GildedRoseTest {
         Assert.assertEquals(50,item.getQuality());
 
     }
+    @Test
+    public void should_quality_51_when_updateQuality_given_item_name_Aged_Brie_sellIn_10_and_quality_is_51(){
+        Item item = new Item("Aged Brie",10,51);
+        GildedRose gildedRose = new GildedRose(singletonList(item));
+
+        gildedRose.updateQuality();
+
+        Assert.assertEquals(51,item.getQuality());
+
+    }
+    @Test
+    public void should_quality_50_when_updateQuality_given_item_name_Backstage_passes_to_a_TAFKAL80ETC_concert_sellIn_10_and_quality_is_48(){
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert",10,48);
+        GildedRose gildedRose = new GildedRose(singletonList(item));
+
+        gildedRose.updateQuality();
+
+        Assert.assertEquals(50,item.getQuality());
+
+    }
+    @Test
+    public void should_quality_49_when_updateQuality_given_item_name_Backstage_passes_to_a_TAFKAL80ETC_concert_sellIn_12_and_quality_is_48(){
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert",12,48);
+        GildedRose gildedRose = new GildedRose(singletonList(item));
+
+        gildedRose.updateQuality();
+
+        Assert.assertEquals(49,item.getQuality());
+
+    }
 }
